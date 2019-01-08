5e60e8c38331e22c8702bbf74515e95b212d8ddc
Andreas Scholten <mail@ascholten.de>
2019-01-08 08:18:14 +0100

    Bomb #1
    Assets

---
+++ Assets/0_SchutzengelProjekt/Scripts/PeopleController/Gunner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gunner : MonoBehaviour
+{
+
+    private Animator animator;
+    public bool gun = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.animator = this.GetComponent<Animator>();
+        if (this.animator != null)
+        {
+            if (this.gun) this.start();
+        }
+    }
+
+    public void stop()
+    {
+        gun = false;
+        this.animator.SetInteger("WeaponType_int", 0);
+    }
+
+    public void start()
+    {
+        gun = true;
+        this.animator.SetFloat("Speed_f", 0.0f);
+        this.animator.SetBool("Grounded", true);
+        this.animator.SetBool("Static_b", true);
+        this.animator.SetInteger("Animation_int", 0);
+        this.animator.SetInteger("WeaponType_int", 8);
+    }
+}
